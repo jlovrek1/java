5a2485e009b480fbedcb3047e8aaa0b00a532db7
jlovrek1 <josephlovrekovic@gmail.com>
2024-04-28 00:29:06 -0400

    started java calc app

---
+++ calculator/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+
+public class App {
+     
+    public static void main(String[] args) throws Exception {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter 1st value");
+        String firstValue = userInput.nextLine();
+    }
+}
