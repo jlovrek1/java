7a35a8af65c8e8c67b0b2e408b147870c63823ab
jlovrek1 <josephlovrekovic@gmail.com>
2024-04-28 15:03:27 -0400

    more calc

--- calculator/src/App.java
+++ calculator/src/App.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
 public class App {
      
     public static void main(String[] args) throws Exception {
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter 1st value");
-        String firstValue = userInput.nextLine();
+         
+         
+         Double number1, number2, result;
+         try (Scanner userInput = new Scanner(System.in)) {
+            System.out.println("Enter your operator");
+            char operator = userInput.next().charAt(0);
+            System.out.println("Enter 1st value");
+            number1 = userInput.nextDouble();
+            number2 = userInput.nextDouble();
+            switch (operator){
+                case '+':
+                result = number1 + number2;
+                case '-':
+                result = number1 - number2;
+                case '*':
+                result = number1 * number2; 
+                case '/':
+                result = number1 / number2;
+
+            }
+        }
+         System.out.println("Enter 2nd value");
     }
 }
